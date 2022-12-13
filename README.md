ebcdb2811103e59331b3681b58f06a0d0af5e78a
brito101 <rodrigobrito101@hotmail.com>
2022-12-13 10:52:56 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Base app Laravel framework version 9
---
+++ routes/web.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\Admin\{
+    AdminController,
+    ACL\PermissionController,
+    ACL\RoleController,
+    UserController
+};
+use Illuminate\Support\Facades\{
+    Auth,
+    Route
+};
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('admin', [AdminController::class, 'index'])->name('admin.home');
+    Route::prefix('admin')->name('admin.')->group(function () {
+        /** Chart home */
+        Route::get('/chart', [AdminController::class, 'chart'])->name('home.chart');
+
+        /** Users */
+        Route::get('/user/edit', [UserController::class, 'edit'])->name('user.edit');
+        Route::get('/users/destroy/{id}', [UserController::class, 'destroy']);
+        Route::resource('users', UserController::class);
+
+        /**
+         * ACL
+         * */
+        /** Permissions */
+        Route::get('/permission/destroy/{id}', [PermissionController::class, 'destroy']);
+        Route::resource('permission', PermissionController::class);
+        /** Roles */
+        Route::get('/role/destroy/{id}', [RoleController::class, 'destroy']);
+        Route::get('role/{role}/permission', [RoleController::class, 'permissions'])->name('role.permissions');
+        Route::put('role/{role}/permission/sync', [RoleController::class, 'permissionsSync'])->name('role.permissionsSync');
+        Route::resource('role', RoleController::class);
+    });
+});
+
+/** Web */
+/** Home */
+// Route::get('/', [SiteController::class, 'index'])->name('home');
+Route::get('/', function () {
+    return redirect('admin');
+});
+
+Auth::routes();
+
+Route::fallback(function () {
+    return view('404');
+});
